efb0991ef80d9dfd80173dea299e0b7af86de321
daniel egeonu <degeonu23@gmail.com>
2020-06-14 18:39:19 +0100

    javascript

---
+++ java.js
@@ -0,0 +1,12 @@
+const hamburger = document.querySelector('.hamburger');
+const navLink = document.querySelector('.nav-link');
+const link = document.querySelectorAll('.nav-link li');
+
+
+
+hamburger.addEventListener("click",() => {
+	navLink.classList.toggle("open");
+	link.forEach(link => {
+		link.classList.toggle("fade");
+	});
+});
